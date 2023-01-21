8d958fb54aecc00b4c96c1f4da570e6f2f158d41
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2023-01-21 16:09:22 +0100

    Minor styling issues fixed + JS clean

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-  document.querySelector(".splash").classList.add("display-none");
-});
+const splashScreen = () => {
+  document.addEventListener("DOMContentLoaded", (e) => {
+    document.querySelector(".splash").classList.add("display-none");
+  });
+};
+splashScreen();
 
 $(document).ready(function () {
   $("li").on("click", function () {
