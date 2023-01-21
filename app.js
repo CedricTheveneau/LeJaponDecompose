1da225da2435f1a6f8a08fe4a2d7d75d77a31416
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2023-01-21 13:50:46 +0100

    Fixed some display issues with navbar + cleaner JS

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const splash = document.querySelector(".splash");
-
 document.addEventListener("DOMContentLoaded", (e) => {
-  setTimeout(() => {
-    splash.classList.add("display-none");
-  }, 2000);
+  document.querySelector(".splash").classList.add("display-none");
 });
 
 $(document).ready(function () {
